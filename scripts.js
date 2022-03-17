2ea1038fb35893961245d725b285631e424ff39e
Ethan Hearn <hearn@Ethans-MacBook-Pro.local>
2022-03-17 10:14:23 -0400

    canvas changed from flexbox to grid for more consistent layout

--- scripts.js
+++ scripts.js
@@ -1,33 +1,53 @@
 // Function that construct the grid within the canvas.
+const canvas = document.querySelector("#canvas")
+
+
 
 const grid = (x) => {
+
+    canvas.style.gridTemplateColumns = `repeat(${x}, 1fr)`
+
     for (let cell=0;cell<x*x;cell++) {
         let block = document.createElement('div');
         block.className = 'cell';
         block.style.width = `${600/x}px`;
         block.style.height = `${600/x}px`;
-        document.querySelector('#canvas').appendChild(block);
+        canvas.appendChild(block);
     }
+    const cells = document.querySelectorAll(".cell");
+    for (let i=0; i<cells.length; i++) {
+        cells[i].addEventListener("mouseenter", function(event) {
+            event.target.style.backgroundColor = "#33ff33";
+    })
+}
 }
 
 grid(16);
 
 // Add mouse over events to each cell to change color.
-const cells = document.querySelectorAll(".cell");
 
-for (let i=0; i<cells.length; i++) {
-    cells[i].addEventListener("mouseenter", function(event) {
-        event.target.style.backgroundColor = "black";
-    })
-}
 
-// Function that clears canvas assigned to clear button
+// Function that clears canvas assigned to clear button, then builds new canvas according to size requested
 
 const freshCanvas = () => {
-    
+    const cells = document.querySelectorAll(".cell");
+
     cells.forEach(cell => {
-        cell.style.backgroundColor = "white"
+        cell.style.backgroundColor = "black"
     })
+
+    let size = prompt("How many cells per side? (max 100)");
+
+    // if (size%2!=0) {
+    //     size++;
+    // }
+
+    cells.forEach(cell => {
+        canvas.removeChild(cell);
+    })
+
+    grid(size);
+    
 }
 
 const clear = document.getElementById('clear');
